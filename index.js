9749254e0115125589873f91c1b8151ff60686d5
wsonly <66416267+wsonly@users.noreply.github.com>
2020-06-11 21:13:21 +0800

    将文本内容提出为一个常量

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-var text = document.getElementById("text-id");
+	var text1 = "遇见你我变得很低很低，一直低到尘埃里。";
+	var text2 = "和自己相处，就是让我们静静坐下来，卸下负担，抚慰心灵，清楚自己身在何处、心往何方。";
+	var text3 = "软弱的人被生活折磨，强悍的人折磨生活。";
+	var text4 = "世上最有趣的人生，不是你降生在一个最有趣的世界，而是你找到一种最有趣的活法。";
+	var text5 = "年年岁岁，平凡的轮回，犹如这美好的绿色。心在平静里，简单，真实，自在，空心。";
+	
+	var text = document.getElementById("text-id");
     var image = document.getElementById("main-id");
-    var btn1 =document.getElementById("btn1").addEventListener("mouseover",image1);
+    
+	var btn1 =document.getElementById("btn1").addEventListener("mouseover",image1);
     var btn2 =document.getElementById("btn2").addEventListener("mouseover",image2);
     var btn3 =document.getElementById("btn3").addEventListener("mouseover",image3);
     var btn4 =document.getElementById("btn4").addEventListener("mouseover",image4);
     var btn5 =document.getElementById("btn5").addEventListener("mouseover",image5);
+	
    function image1(){
-      text.innerHTML =  "遇见你我变得很低很低，一直低到尘埃里。";
+      text.innerHTML =  text1;
       image.style.backgroundImage = "URL('background-image/01.jpg')";
    }
    function image2(){
-      text.innerHTML =  "和自己相处，就是让我们静静坐下来，卸下负担，抚慰心灵，清楚自己身在何处、心往何方。";
+      text.innerHTML =  text2;
       image.style.backgroundImage = "URL('background-image/02.jpg')";
    }
    function image3(){
-      text.innerHTML =  "软弱的人被生活折磨，强悍的人折磨生活。";
+      text.innerHTML = text3 ;
       image.style.backgroundImage = "URL('background-image/03.jpg')";
    }
    function image4(){
-      text.innerHTML =  "世上最有趣的人生，不是你降生在一个最有趣的世界，而是你找到一种最有趣的活法。";
+      text.innerHTML = text4 ;
       image.style.backgroundImage = "URL('background-image/04.jpg')";
    }
    function image5(){
-      text.innerHTML =  "年年岁岁，平凡的轮回，犹如这美好的绿色。心在平静里，简单，真实，自在，空心。";
+      text.innerHTML = text5 ;
       image.style.backgroundImage = "URL('background-image/05.jpg')";
    }
+   
    
